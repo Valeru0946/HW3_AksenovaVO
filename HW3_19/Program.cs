45d937d06991a76b6f825619cbc76a0767bfe790
Valerya <blerao@icloud.com>
2023-01-11 15:53:09 +0300

    19

---
+++ HW3_19/Program.cs
@@ -0,0 +1,19 @@
+﻿//зАДАЧА 19
+Console.WriteLine("Введите число");
+string number = Console.ReadLine ();
+int len = number.Length ;
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+      Console.WriteLine("Палиндром");  
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("не пятизначное число");
+}
