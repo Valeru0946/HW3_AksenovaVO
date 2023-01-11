7a1c284fc0e8b119aebc2c11d3b6826821b23a40
Valerya <blerao@icloud.com>
2023-01-11 15:52:27 +0300

    21

---
+++ HW3_21/Program.cs
@@ -0,0 +1,18 @@
+﻿//задача 21
+int x1 = ReadInt ("Введите координату X1");
+int y1 = ReadInt ("Введите координату Y1");
+int z1 = ReadInt ("Введите координату Z1");
+int x2 = ReadInt ("Введите координату X2");
+int y2 = ReadInt ("Введите координату Y2");
+int z2 = ReadInt ("Введите координату Z2");
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+double length = Math.Sqrt (A * A + B * B + C * C);
+Console.WriteLine ($"расстояние между точками { length }");
+int ReadInt (string message)
+{
+    Console.Write( message );
+    return
+Convert.ToInt32 (Console.ReadLine ());
+}
